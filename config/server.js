2d1ba4b25995dfe6427bf0935196fb7b3b2bd976
Pblonghurst <59968417+Phil-Longhurst@users.noreply.github.com>
2023-04-03 00:11:28 +0100

    serv

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'http://34.142.81.181'),
   app: {
     keys: env.array('APP_KEYS'),
   },
