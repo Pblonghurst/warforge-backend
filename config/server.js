c3f01f8709c38616ed000346ab4c433dcced1a13
Pblonghurst <59968417+Phil-Longhurst@users.noreply.github.com>
2023-04-03 00:23:38 +0100

    final

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://34.142.81.181'),
+  url: env('PUBLIC_URL', 'http://34.142.81.181:1337'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '4fb050c5d29890ba3fc71f114e18a432bb39b1425fbde79839bffac3ae02c97a64b3ee19af16b13ec9df9cd6c34a4936f7484545f4be4da3d88ba9b3c6be781e'),
